74770431b229e8180a86f488cc3997b41e5e3517
thinhpxp <thinhpxp@gmail.com>
2025-08-01 14:43:57 +0700

    const userText = t('realtimeUserFrom', '{count} người dùng từ').replace('{count}', location.count); thanh  const userText = t('realtimeUserFrom', '{count}').replace('{count}', location.count);

--- script.js
+++ script.js
@@ -450,7 +450,7 @@ async function displayRealtimeLocations() {
                 }
                 // =======================================================
 
-                const userText = t('realtimeUserFrom', '{count} người dùng từ').replace('{count}', location.count);
+                const userText = t('realtimeUserFrom', '{count}').replace('{count}', location.count);
 
                 li.innerHTML = `${userText} <strong>${locationDisplay}</strong>`;
                 listElement.appendChild(li);
